634f31ae7a4a233b268095dd7631f02b9371401b
Varun <varunbhouria78@gmail.com>
2020-06-12 20:16:54 +0530

    error fixed: cannot get /private

--- Practice-Digital_flavours/server.js
+++ Practice-Digital_flavours/server.js
@@ -16,6 +16,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use('/', express.static (path.join (__dirname, "public")))
+app.use('/public',require('./routes/public')) //route doesnt specify the routes to /public and /private, you app.use them here
+app.use('/private',require('./routes/private'))
 app.use('/',require('./routes').route)
 
 app.listen(2424, ()=>{console.log("Server stated at http://localhost:2424")})
